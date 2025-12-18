fb25d1534781ea98185eabd91eb93f7a7af5cc0c
lockxii <lockxiiii@gmail.com>
2025-12-19 00:42:50 +0100

    fix: Correction du chargement du tableau avec ClientSideSuspense et écran de chargement

--- components/room.tsx
+++ components/room.tsx
@@ -17,13 +17,17 @@ interface RoomProps {
 
 export function Room({ children, roomId, template, title }: RoomProps) {
   return (
-    <RoomProvider id={roomId} initialPresence={{ selection: [], cursor: null }} initialStorage={{
-      layers: new LiveMap<string, LiveObject<Layer>>(),
-      layerIds: new LiveList([]),
-      auditLog: new LiveList([]),
-    }}>
-      <ClientSideSuspense fallback={<div>Loading…</div>}>
-        <Canvas template={template} title={title} />
+    <RoomProvider 
+        id={roomId} 
+        initialPresence={{ selection: [], cursor: null }} 
+        initialStorage={{
+            layers: new LiveMap<string, LiveObject<Layer>>(),
+            layerIds: new LiveList([]),
+            auditLog: new LiveList([]),
+        }}
+    >
+      <ClientSideSuspense fallback={<Loading />}>
+        {() => <Canvas template={template} title={title} />}
       </ClientSideSuspense>
     </RoomProvider>
   );
