ce9a3b442b6cbc82685023169723148f332e14ef
lockxii <lockxiiii@gmail.com>
2026-01-20 20:34:41 +0100

    Restore stable Prisma connection

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,6 +3,11 @@ import { PrismaClient } from '@prisma/client'
 const prismaClientSingleton = () => {
   return new PrismaClient({
     log: ['error'],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL
+      }
+    }
   })
 }
 
